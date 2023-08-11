61e9b342a99c2699028cbe0575f2765016f3c6da
kruthi24 <kruthi24p3@gmail.com>
2023-08-12 00:15:44 +0530

    fixed ra and dec in plotTiles output table

--- sky_tiling/ranked_tiling.py
+++ sky_tiling/ranked_tiling.py
@@ -374,7 +374,7 @@ class RankedTileGenerator:
 			pl.show()
 		
 		ranks = np.arange(1, len(ranked_tile_indices)+1)
-		output = np.vstack((ranks, ranked_tile_indices, RA_tile[include_tiles], Dec_tile[include_tiles], ranked_tile_probs)).T
+		output = np.vstack((ranks, ranked_tile_indices, RA_tile[ranked_tile_indices.astype(int)], Dec_tile[ranked_tile_indices.astype(int)], ranked_tile_probs)).T
 		t = Table(rows=output, names=('Rank', 'index', 'RA', 'Dec', 'Probability'), dtype=('i4', 'i4', 'f8', 'f8', 'f8'))
 		return t 
 
